bf5d957c202c2ef27067234b0378d4d51a352dc2
Alper Alkan <alper.alkan2000@gmail.com>
2024-01-30 00:59:35 +0100

    example plugin

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# gamevault-backend-example-plugin
-This is a sample plugin repository that demonstrates how to write GameVault plugins.
+# Example Plugin for GameVault Backend
+
+This repository provides a simple example of a plugin to guide you in creating GameVault plugins.
+
+Check out each plugin file to understand its functioning.
+
+To activate a plugin on your GameVault Server, set `PLUGIN_ENABLED` to `true` and include the raw URL of the plugin's source in the `PLUGIN_SOURCES` list in your server configuration.
---
+++ plugins/gamevault-chuck-norris-joke-fetcher-plugin.js
@@ -0,0 +1,39 @@
+/*
+ * Chuck Norris Joke Fetcher
+ *
+ * This plugin shows how to run arbitrary JavaScript code plugins in a gamevault-backend.
+ * It will get executet as soon as the plugin is loaded.
+ *
+ * This plugin fetches a random Chuck Norris joke from the Chuck Norris API and logs it to the console.
+ * It uses the native JavaScript `fetch` API to make a network request to the Chuck Norris API endpoint.
+ * The fetched joke is then extracted from the API response and logged to the console.
+ * Error handling is implemented to deal with network issues or problems with the API response.
+ *
+ */
+
+export const meta = {
+  name: "Chuck Norris Joke Fetcher",
+  description: "Fetches a random Chuck Norris joke and logs it to the console.",
+  version: "1.0.0",
+  author: "Alfagun74",
+};
+
+function fetchChuckNorrisJoke() {
+  const apiUrl = "https://api.chucknorris.io/jokes/random";
+
+  fetch(apiUrl)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      return response.json();
+    })
+    .then((data) => {
+      console.log("Chuck Norris Joke:", data.value);
+    })
+    .catch((error) => {
+      console.error("Error fetching Chuck Norris joke:", error.message);
+    });
+}
+
+fetchChuckNorrisJoke();
